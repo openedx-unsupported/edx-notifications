4ae2b15c4a4626e12a602378cea579a6273b0b67
Chris Dodge <cdodge@edx.org>
2015-02-18 21:36:31 -0500

    add the RequireJS namespacing trick that the LMS uses, to make it more consistent with edx-platform

---
+++ testserver/static/js/RequireJS-namespace.js
@@ -0,0 +1,5 @@
+window.RequireJS = window.RequireJS || {};
+RequireJS.requirejs = RequireJS.requirejs || window.requirejs;
+RequireJS.require = RequireJS.require || window.require;
+RequireJS.define = RequireJS.define || window.define;
+RequireJS.config = RequireJS.config || window.require.config;
