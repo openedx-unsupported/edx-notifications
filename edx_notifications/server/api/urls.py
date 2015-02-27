3cb6582c5f9192059395a5e0b8c01e5c61fcc48c
Chris Dodge <cdodge@edx.org>
2015-02-27 16:47:54 -0500

    add mock urls for the API

--- edx_notifications/server/api/urls.py
+++ edx_notifications/server/api/urls.py
@@ -7,30 +7,39 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from edx_notifications.server.api import consumer as consumer_views
 
+from .url_regex import (
+    CONSUMER_NOTIFICATIONS_COUNT_REGEX,
+    CONSUMER_NOTIFICATION_DETAIL_REGEX,
+    CONSUMER_NOTIFICATIONS_MARK_NOTIFICATIONS_REGEX,
+    CONSUMER_NOTIFICATIONS_REGEX,
+    CONSUMER_RENDERERS_TEMPLATES_REGEX,
+)
+
+
 urlpatterns = patterns(  # pylint: disable=invalid-name
     '',
     url(
-        r'edx_notifications/v1/consumer/notifications/count$',
+        CONSUMER_NOTIFICATIONS_COUNT_REGEX,
         consumer_views.NotificationCount.as_view(),
         name='edx_notifications.consumer.notifications.count'
     ),
     url(
-        r'edx_notifications/v1/consumer/notifications/(?P<msg_id>[0-9]+)$',
+        CONSUMER_NOTIFICATION_DETAIL_REGEX,
         consumer_views.NotificationDetail.as_view(),
         name='edx_notifications.consumer.notifications.detail'
     ),
     url(
-        r'edx_notifications/v1/consumer/notifications/mark_notifications$',
+        CONSUMER_NOTIFICATIONS_MARK_NOTIFICATIONS_REGEX,
         consumer_views.MarkNotifications.as_view(),
         name='edx_notifications.consumer.notifications.mark_notifications'
     ),
     url(
-        r'edx_notifications/v1/consumer/notifications$',
+        CONSUMER_NOTIFICATIONS_REGEX,
         consumer_views.NotificationsList.as_view(),
         name='edx_notifications.consumer.notifications'
     ),
     url(
-        r'edx_notifications/v1/consumer/renderers/templates$',
+        CONSUMER_RENDERERS_TEMPLATES_REGEX,
         consumer_views.RendererTemplatesList.as_view(),
         name='edx_notifications.consumer.renderers.templates'
     ),
