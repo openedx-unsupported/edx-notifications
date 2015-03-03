d6014c139def2c99c53317060f06f4e76cbbb5ce
kashifch <kchaudhry@edx.org>
2015-03-03 19:03:24 +0500

    some more jasmine tests

--- edx_notifications/server/web/static/edx_notifications/js/test/spec/counter_icon_view_Spec.js
+++ edx_notifications/server/web/static/edx_notifications/js/test/spec/counter_icon_view_Spec.js
@@ -21,8 +21,8 @@ describe("CounterIconView", function(){
 
             endpoints: {
                 unread_notification_count: "/count/unread",
-                user_notifications: "",
-                renderer_templates_urls: ""
+                user_notifications: "/notification/pane",
+                renderer_templates_urls: "/renderer/templates"
             },
             view_templates: {
                 notification_icon: NotificationIcon,
@@ -32,6 +32,7 @@ describe("CounterIconView", function(){
                 name: "none"
             }
         });
+        this.counter_view.render();
     });
 
     afterEach(function() {
@@ -42,8 +43,28 @@ describe("CounterIconView", function(){
         expect(this.counter_view.model.url).toBe("/count/unread")
     });
 
+    it("should return unread_notification_count url in endpoint", function(){
+        expect(this.counter_view.endpoints.unread_notification_count).toEqual('/count/unread');
+    });
+
+    it("should return user_notifications url in endpoint", function(){
+        expect(this.counter_view.endpoints.user_notifications).toBe('/notification/pane');
+    });
+
+    it("should return renderer_templates_urls in endpoints", function(){
+        expect(this.counter_view.endpoints.renderer_templates_urls).toEqual('/renderer/templates');
+    });
+
+    it('get html for notification icon', function(){
+        expect(this.counter_view.view_templates.notification_icon.split(" ")).toContain("count");
+    });
+
+    it('get html for notification pane', function(){
+        expect(this.counter_view.view_templates.notification_pane).toContain("Notifications");
+    });
+
     it("get unread notifications count", function(){
-        var unread_count = 2030
+        var unread_count = 2030;
         this.server.respondWith(
             "GET",
             "/count/unread",
@@ -55,6 +76,14 @@ describe("CounterIconView", function(){
         );
         this.server.respond();
         expect(this.counter_view.$el.html()).toContain(unread_count)
+    });
+
+    it("should call showPane function on clicking notification icon", function(){
+        var target = $(".edx-notifications-icon");
+        var showPaneSpy = spyOn(this.counter_view, 'showPane');
+        this.counter_view.delegateEvents();
+        target.click();
+        expect(showPaneSpy).toHaveBeenCalled();
     });
   });
 });
