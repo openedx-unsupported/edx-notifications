8450580b6d79979b015f8fd61cef71d909bc3db0
kashifch <kchaudhry@edx.org>
2015-03-08 01:20:43 +0500

    correct url

--- testserver/test/acceptance/pages/__init__.py
+++ testserver/test/acceptance/pages/__init__.py
@@ -1,4 +1,4 @@
-base_url = 'http://71c52982.ngrok.com'
-user_name = 'testuser3'
+base_url = 'http://127.0.0.1:8000/'
+user_name = 'testuser1'
 user_email = 'testuser1@edx.org'
 password = 'ARbi12.,'
