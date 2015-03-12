e5e8ecd392108601ebf621c54df55620a277f401
kashifch <kchaudhry@edx.org>
2015-03-13 03:27:20 +0500

    removed --with-coverage

--- run_bokchoy_tests.sh
+++ run_bokchoy_tests.sh
@@ -42,7 +42,7 @@ until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8000); d
 done
 
 echo "Running acceptance tests..."
-nosetests testserver/test/acceptance --with-coverage
+nosetests testserver/test/acceptance
 
 # capture the exit code from the test.  Anything more than 0 indicates failed cases.
 EXIT_CODE=$?
