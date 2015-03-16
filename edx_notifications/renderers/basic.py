d12a39cc2dfa61b1e786b5f968114f69299c8c14
Afzal Wali <afzal@edx.org>
2015-03-16 19:31:59 +0500

    new file for digest email renderers.

--- edx_notifications/renderers/basic.py
+++ edx_notifications/renderers/basic.py
@@ -8,7 +8,7 @@ from edx_notifications.renderers.renderer import BaseNotificationRenderer
 
 from edx_notifications.const import (
     RENDER_FORMAT_UNDERSCORE,
-    RENDER_FORMAT_DIGEST)
+)
 
 
 def path_to_underscore_template(name):
@@ -20,17 +20,6 @@ def path_to_underscore_template(name):
     return static(
         'edx_notifications/templates/renderers/{name}'.format(name=name)
     )
-
-
-def path_to_digest_template(name):
-    """
-    Helper to construct a full path to where we have
-    system defined Digest Email rendering templates
-    """
-    pass #TODO have to figure it out
-    # return static(
-    #     'edx_notifications/templates/renderers/{name}'.format(name=name)
-    # )
 
 
 class UnderscoreStaticFileRenderer(BaseNotificationRenderer):
@@ -75,52 +64,7 @@ class UnderscoreStaticFileRenderer(BaseNotificationRenderer):
         """
 
         if render_format == RENDER_FORMAT_UNDERSCORE and self.underscore_template_name:
-            return path_to_digest_template(self.digest_notification_template_name)
-
-        raise NotImplementedError()
-
-
-class EmailDigestTemplateRenderer(BaseNotificationRenderer):
-    """
-    This subclass of BaseNotification only provides an Email Digest renderer.
-    These templates will be used to render html for sending out Digest emails.
-    """
-
-    digest_notification_template_name = None
-
-    def __init__(self, template_name=None):
-        """
-        Initializer
-        """
-        if template_name:
-            self.digest_notification_template_name = template_name
-
-    def can_render_format(self, render_format):
-        """
-        Returns (True/False) whether this renderer provides renderings
-        into the requested format.
-        """
-        return render_format == RENDER_FORMAT_DIGEST
-
-    def render_subject(self, msg, render_format, lang):
-        """
-        This basic renderer just returns the subject in the Msg payload
-        """
-        return msg.payload['subject']
-
-    def render_body(self, msg, render_format, lang):
-        """
-        This basic renderer just returns the  body that is in the Msg payload
-        """
-        return msg.payload['body']
-
-    def get_template_path(self, render_format):
-        """
-        Return a path to where a client can get the template
-        """
-
-        if render_format == RENDER_FORMAT_DIGEST and self.digest_notification_template_name:
-            return path_to_underscore_template(self.digest_notification_template_name)
+            return path_to_underscore_template(self.underscore_template_name)
 
         raise NotImplementedError()
 
