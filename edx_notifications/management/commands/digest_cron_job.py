9e4e3e6db7641053b5ca9fbe0cbd5418fb4b0add
Muhammad Shoaib <muammad.shoaib@arbisoft.com>
2015-03-20 18:39:19 +0500

    (WIP) work for renderings of the digest email templates in the same manner as backbone rendering of underscore template for notifications

--- edx_notifications/management/commands/digest_cron_job.py
+++ edx_notifications/management/commands/digest_cron_job.py
@@ -4,7 +4,8 @@ application-level listeners
 """
 
 import logging
-
+from itertools import groupby
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.template import Context, loader
@@ -16,17 +17,129 @@ from edx_notifications.renderers.renderer import (
 )
 from edx_notifications import const
 
+# this describes how we want to group together notification types into visual groups
+grouping_config = {
+    'groups': {
+        'announcements': {
+            'name': 'announcements',
+            'display_name': 'Announcements',
+            'group_order': 1
+        },
+        'group_work': {
+            'name': 'group_work',
+            'display_name': 'Group Work',
+            'group_order': 2
+        },
+        'leaderboards': {
+            'name': 'leaderboards',
+            'display_name': 'Leaderboards',
+            'group_order': 3
+        },
+        'discussions': {
+            'name': 'discussions',
+            'display_name': 'Discussion',
+            'group_order': 4
+        },
+        '_default': {
+            'name': '_default',
+            'display_name': 'Other',
+            'group_order': 5
+        },
+    },
+    'type_mapping': {
+        'open-edx.lms.discussions.cohorted-thread-added': 'group_work',
+        'open-edx.lms.discussions.cohorted-comment-added': 'group_work',
+        'open-edx.lms.discussions.*': 'discussions',
+        'open-edx.lms.leaderboard.*': 'leaderboards',
+        'open-edx.studio.announcements.*': 'announcements',
+        'open-edx.xblock.group-project.*': 'group_work',
+        '*': '_default'
+    },
+}
 
-def get_paths_for_digest_templates():
+
+def get_path_for_digest_templates():
     """
-
-    :return:
+    returns a list of all digest format templates that have been registered in the system.
     """
     result_dict = {}
     for class_name, renderer in get_all_renderers().iteritems():
         if renderer['digest_renderer_instance'].can_render_format(const.RENDER_FORMAT_DIGEST):
             result_dict[class_name] = renderer['digest_renderer_instance'].get_template_path(const.RENDER_FORMAT_DIGEST)
     return result_dict
+
+
+def get_group_name_for_msg_type(msg_type):
+    """
+    Returns the particular group_name for the msg_type
+    else return None if no group_name is found.
+    """
+    if msg_type in grouping_config['type_mapping']:
+        group_name = grouping_config['type_mapping'][msg_type]
+        if group_name in grouping_config['groups']:
+            return group_name
+
+    # no exact match so lets look upwards for wildcards
+    search_type = msg_type
+    dot_index = search_type.rindex('.')
+    while dot_index != -1 and search_type != '*':
+        search_type = search_type[0: dot_index]
+        key = search_type + '.*'
+
+        if key in grouping_config['type_mapping']:
+            group_name = grouping_config['type_mapping'][key]
+            if group_name in grouping_config['groups']:
+                return group_name
+
+        dot_index = search_type.rindex('.')
+
+    # look for global wildcard
+    if '*' in grouping_config['type_mapping']:
+        key = '*'
+        group_name = grouping_config['type_mapping'][key]
+        if group_name in grouping_config['groups']:
+            return group_name
+
+    # this really shouldn't happen. This means misconfiguration
+    return None
+
+
+def get_group_rendering(group_data):
+    """
+    returns the list of the sorted user notifications renderings.
+    """
+    renderings = []
+    result_dict = get_path_for_digest_templates()
+    group_data = sorted(group_data, key=lambda k: k['msg']['created'])
+
+    for user_msg in group_data:
+        msg = user_msg['msg']
+        render_context = msg['payload']
+        created_date = msg['created']
+
+        if datetime.date(created_date) == datetime.today().date():
+            created_str = 'Today at ' + created_date.strftime("%H:%M%p")
+        else:
+            created_str = created_date.strftime("%B %d, %Y") + ' at ' + created_date.strftime("%H:%M%p")
+
+        render_context.update({
+            'display_created': created_str
+        })
+        renderer_class_name = msg['msg_type']['renderer']
+        if renderer_class_name in result_dict:
+            context = Context(render_context)
+            template = loader.get_template(result_dict[renderer_class_name])
+            notification_html = template.render(context)
+            renderings.append({
+                'user_msg': user_msg,
+                'msg': msg,
+                # render the particular NotificationMessage
+                'html': notification_html,
+                'group_name': get_group_name_for_msg_type(msg['msg_type']['name'])
+            })
+
+    return renderings
+
 
 class Command(BaseCommand):
     """
@@ -37,22 +150,39 @@ class Command(BaseCommand):
         """
         Management command entry point, simply call into the signal firiing
         """
-        print('Hello World')
+        # initialize the notification subsystem
         startup.initialize()
-        result_dict = get_paths_for_digest_templates()
-        render_templates = {}
+
         # get all the users
         users = User.objects.all()
+
         for user in users:
             # now get all the user notifications.
-            html = []
+            notification_groups = []
             user_msgs = get_notifications_for_user(
                 int(user.id)
             )
             user_email = user.email
-            resultset = [user_msg.get_fields() for user_msg in user_msgs]
-            for result in resultset:
-                if result['msg']['msg_type']['renderer'] in result_dict:
-                    context = Context(result['msg']['payload'])
-                    template = loader.get_template(result_dict[result['msg']['msg_type']['renderer']])
-                    template.render(context)
+            result_set = [user_msg.get_fields() for user_msg in user_msgs]
+            grouped_data = {}
+
+            # group the user notifications by message type name.
+            for key, group in groupby(result_set, lambda x: get_group_name_for_msg_type(x['msg']['msg_type']['name'])):
+                for thing in group:
+                    if key in grouped_data:
+                        grouped_data[key].append(thing)
+                    else:
+                        grouped_data[key] = [thing]
+
+            # then we want to order the groups according to the grouping_config
+            # so we can specify which groups go up at the top
+            group_orderings = sorted(grouping_config['groups'].items(), key=lambda t: t[1]['group_order'])
+
+            for group_key, _ in group_orderings:
+                if group_key in grouped_data:
+                    notification_groups.append(
+                        {
+                            'group_title': grouping_config['groups'][group_key]['display_name'],
+                            'messages': get_group_rendering(grouped_data[group_key])
+                        }
+                    )
