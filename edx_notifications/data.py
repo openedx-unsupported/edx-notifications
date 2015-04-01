809b5397c4c162df824a6cd1319543c31ab7b7c6
Muhammad Shoaib <muammad.shoaib@arbisoft.com>
2015-04-01 17:54:53 +0500

    added the new column named 'default value'

--- edx_notifications/data.py
+++ edx_notifications/data.py
@@ -329,6 +329,9 @@ class NotificationPreference(BaseDataObject):
     # display description for notification preference
     display_description = StringField()
 
+    # default_value for notification preference
+    default_value = StringField()
+
 
 class UserNotificationPreferences(BaseDataObject):
     """
