97cc2daea4b359715cf883a188562a2248bf728a
Afzal Wali <afzal@edx.org>
2015-04-07 15:46:54 +0500

    Implemented the publisher's purge_expired_notifications() Moved around constants. Added configuration variables to settings and consts.

--- edx_notifications/lib/publisher.py
+++ edx_notifications/lib/publisher.py
@@ -15,6 +15,7 @@ from contracts import contract
 from django.db.models.query import ValuesListQuerySet
 
 from edx_notifications.channels.channel import get_notification_channel
+from edx_notifications import const
 from edx_notifications.stores.store import notification_store
 from edx_notifications.exceptions import ItemNotFoundError
 
@@ -329,4 +330,21 @@ def purge_expired_notifications():
     can remain in the system before being purged. Lack of configuration (or None) means "don't purge ever"
     and calls into the store provider's purge_expired_notifications() method.
     """
-    pass
+
+    store = notification_store()
+    now = datetime.datetime.now(pytz.UTC)
+
+    if const.PURGE_READ_NOTIFICATIONS_OLDER_THAN_DAYS:
+        purge_read_older_than = now - datetime.timedelta(days=const.PURGE_READ_NOTIFICATIONS_OLDER_THAN_DAYS)
+    else:
+        purge_read_older_than = None
+
+    if const.PURGE_UNREAD_NOTIFICATIONS_OLDER_THAN_DAYS:
+        purge_unread_older_than = now - datetime.timedelta(days=const.PURGE_UNREAD_NOTIFICATIONS_OLDER_THAN_DAYS)
+    else:
+        purge_unread_older_than = None
+
+    store.purge_expired_notifications(
+        purge_read_messages_older_than=purge_read_older_than,
+        purge_unread_messages_older_than=purge_unread_older_than
+    )
