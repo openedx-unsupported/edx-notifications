9c03086dd1bb42ed537a852d0fad42dc1684ca20
Chris Dodge <cdodge@edx.org>
2015-04-07 14:17:16 -0400

    reconfigure logging to go to stdout on management commands

--- edx_notifications/management/commands/background_notification_check.py
+++ edx_notifications/management/commands/background_notification_check.py
@@ -3,7 +3,26 @@ Django management command to raise a 'fire_background_notification_check' signal
 application-level listeners
 """
 
-import logging
+import logging, logging.config
+import sys
+
+# Have all logging go to stdout with management commands
+# this must be up at the top otherwise the
+# configuration does not appear to take affect
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    }
+}
+logging.config.dictConfig(LOGGING)
 
 from django.core.management.base import BaseCommand
 
@@ -23,9 +42,7 @@ class Command(BaseCommand):
         """
 
         log.info("Running management command to fire notifications asynchronously...")
-        print "Running background_notification_check..."
 
         fire_background_notification_check()
 
         log.info("Completed background_notification_check.")
-        print "Completed background_notification_check."
