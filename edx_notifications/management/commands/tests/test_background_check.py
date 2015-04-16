a5a20bdf1f2f3d8f4d570b535e8130f164020b1e
Afzal Wali <afzal@edx.org>
2015-04-16 19:22:32 +0500

    Added a "testcase" that just executes the command for now.

--- edx_notifications/management/commands/tests/test_background_check.py
+++ edx_notifications/management/commands/tests/test_background_check.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 from django.dispatch import receiver
 
-from edx_notifications.management.commands import background_notification_check, force_purge
+from edx_notifications.management.commands import background_notification_check, force_purge, force_send_digest
 from edx_notifications.stores.sql.store_provider import SQLNotificationStoreProvider
 
 from edx_notifications.stores.store import notification_store
@@ -150,3 +150,11 @@ class PurgeNotificationsCommandTest(TestCase):
             ),
             0
         )
+
+
+class ForceSendDigestCommandTest(TestCase):
+    def test_command_check(self):
+        # run the management command for purging notifications.
+        force_send_digest.Command().handle(**{'send_daily_digest': True, 'send_weekly_digest': True, 'namespace': 'All'})
+        force_send_digest.Command().handle(**{'send_daily_digest': True, 'send_weekly_digest': True, 'namespace': 'ABC'})
+        # force_send_digest.Command().handle(**{'send_daily_digest': False, 'send_weekly_digest': False, 'namespace': 'ABC'})
