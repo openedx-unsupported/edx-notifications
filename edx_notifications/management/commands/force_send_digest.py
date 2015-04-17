cead63933a6481302e009270d278570f92033c1b
Afzal Wali <afzal@edx.org>
2015-04-17 16:34:03 +0500

    Added additional logs and comments.

--- edx_notifications/management/commands/force_send_digest.py
+++ edx_notifications/management/commands/force_send_digest.py
@@ -83,11 +83,12 @@ class Command(BaseCommand):
         from_email = const.NOTIFICATION_DIGEST_FROM_ADDRESS
 
         if namespace == "All":
-            send_notifications_digest(from_timestamp, to_timestamp, preference_name, subject, from_email)
+            digests_sent = send_notifications_digest(from_timestamp, to_timestamp, preference_name, subject, from_email)
         else:
-            send_notifications_namespace_digest(
+            digests_sent = send_notifications_namespace_digest(
                 namespace, from_timestamp, to_timestamp, preference_name, subject, from_email
             )
+        return digests_sent
 
     def send_weekly_digest(self, namespace='All'):
         """
@@ -100,15 +101,21 @@ class Command(BaseCommand):
         from_email = const.NOTIFICATION_DIGEST_FROM_ADDRESS
 
         if namespace == "All":
-            send_notifications_digest(from_timestamp, to_timestamp, preference_name, subject, from_email)
+            digests_sent = send_notifications_digest(from_timestamp, to_timestamp, preference_name, subject, from_email)
         else:
-            send_notifications_namespace_digest(
+            digests_sent = send_notifications_namespace_digest(
                 namespace, from_timestamp, to_timestamp, preference_name, subject, from_email
             )
+        return digests_sent
 
     def handle(self, *args, **options):
         """
-        Management command entry point, simply call into the
+        Management command entry point, simply call into the send_notifications_digest or the
+        send_notifications_namespace_digest depending on the passed the parameters.
+        The expected command line arguments are:
+         --daily: Sends the daily digest.
+         --weekly: Sends the weekly digest.
+         --ns=NAMESPACE : Sends the notifications for the particular NAMESPACE.
         """
 
         usage = "usage: %prog [--daily] [--weekly] [--ns=NAMESPACE]"
@@ -117,12 +124,14 @@ class Command(BaseCommand):
         log.info("Running management command ...")
 
         if options['send_daily_digest']:
-            self.send_daily_digest(options['namespace'])
             log.info("Sending the daily digest with namespace=%s...", options['namespace'])
+            weekly_digests_sent = self.send_daily_digest(options['namespace'])
+            log.info("Successfully sent %s digests...", weekly_digests_sent)
 
         if options['send_weekly_digest']:
-            self.send_weekly_digest(options['namespace'])
             log.info("Sending the weekly digest with namespace=%s...", options['namespace'])
+            daily_digests_sent = self.send_weekly_digest(options['namespace'])
+            log.info("Successfully sent %s digests...", daily_digests_sent)
 
         if not options['send_weekly_digest'] and not options['send_daily_digest']:
             parser.print_help()
