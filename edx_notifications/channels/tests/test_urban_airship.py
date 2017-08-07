3efa71a73935114c1bf59dda9ee0919504df682b
Jawad <ranajawadtahir@gmail.com>
2017-08-07 17:49:05 +0500

    Added test cases for UA channel

---
+++ edx_notifications/channels/tests/test_urban_airship.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from edx_notifications import startup
+from edx_notifications.data import (
+    NotificationMessage,
+    NotificationType,
+)
+from edx_notifications.lib.publisher import (
+    register_notification_type,
+    publish_notification_to_tag)
+from testserver.views import CANNED_TEST_PAYLOAD
+
+TEST_TAG = 'cs50'
+
+class UrbanAirTestCases(TestCase):
+    def setUp(self):
+        startup.initialize(register_system_types=False)
+        self.msg_type = NotificationType(
+            name='open-edx.studio.announcements.new-announcement',
+            renderer='edx_notifications.openedx.course_announcements.NewCourseAnnouncementRenderer'
+        )
+        register_notification_type(self.msg_type)
+        self.msg = NotificationMessage(
+            namespace='foo/bar/baz',
+            msg_type=self.msg_type,
+            payload=CANNED_TEST_PAYLOAD['open-edx.studio.announcements.new-announcement']
+        )
+
+    def test_publish_notification_to_tag(self):
+        resp = publish_notification_to_tag(
+            self.msg, 'enrollments', TEST_TAG, 'urban-airship'
+        )
+        self.assertTrue(resp['ok'])
