39e17f7e6778d25662de73d65cf066c33ad9845f
Naeem Ilyas <naeem-ilyas@live.com>
2018-08-20 10:12:35 +0000

    urls fixed

--- testserver/urls.py
+++ testserver/urls.py
@@ -3,14 +3,16 @@ URL mappings for Notifications Server
 """
 
 from django.conf.urls import url, include
+from django.contrib.auth.views import login
 
+import views as testserver_views
 
 urlpatterns = [  # pylint: disable=invalid-name
-    url(r'^$', 'testserver.views.index'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'testserver.views.logout_page'),
-    url(r'^register/$', 'testserver.views.register'),
-    url(r'^register/success/$', 'testserver.views.register_success'),
+    url(r'^$', testserver_views.index),
+    url(r'^accounts/login/$', login),
+    url(r'^logout/$', testserver_views.logout_page),
+    url(r'^register/$', testserver_views.register),
+    url(r'^register/success/$', testserver_views.register_success),
 
     url(r'^api/', include('edx_notifications.server.api.urls')),
 ]
