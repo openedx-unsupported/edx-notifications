d418a0f26b313f027b1f08f1d3f19ea7246937d5
hamzamunir7300 <hamxa.muneer@gmail.com>
2018-11-08 17:35:11 +0500

    adding locale

--- edx_notifications/server/web/urls.py
+++ edx_notifications/server/web/urls.py
@@ -2,11 +2,10 @@
 We currently have no server-side HTML generation. This file is reserved for future use.
 """
 from settings import js_info_dict
-from django.conf.urls import include, url
-from django.views.i18n import javascript_catalog
+from django.conf.urls import include, url  # pylint: disable=unused-import
 
 urlpatterns = [  # pylint: disable=invalid-name
 
-    url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='javascript-catalog'),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='javascript-catalog'),
 
 ]
