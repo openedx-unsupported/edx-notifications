c7d1b513f0a592699628d86254af4bded7ec230f
hamzamunir7300 <hamxa.muneer@gmail.com>
2018-12-05 13:33:48 +0500

    small nit

--- edx_notifications/stores/sql/models.py
+++ edx_notifications/stores/sql/models.py
@@ -133,18 +133,10 @@ class SQLNotificationMessage(TimeStampedModel):
         return msg
 
     def translate_payload_title(self, payload):
-        announcement_date = payload['announcement_date']
-
-        # print "///////////###############//////////////////"
-        # print (_(title[0]))
-        # print (title[0])
-        # title = "{} {} {} {} {}".format(_(title[0]), _(title[1]), title[2], title[3], title[4])
-        # print "///////////###############//////////////////"
-        # print (title)
-
-        title = _('Announcement on {annoucement_date}').format(annoucement_date=announcement_date)
-
-        payload['title'] = title
+        if payload and 'notification_type' in payload and payload['notification_type'] == "courseannouncement":
+            announcement_date = payload['announcement_date']
+            title = _('Announcement on {annoucement_date}').format(annoucement_date=announcement_date)
+            payload['title'] = title
         return payload
 
     @classmethod
